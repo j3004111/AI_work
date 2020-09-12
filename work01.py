3bc98185e7fa6b8a2d93e4e3f217ce436b379cb4
j3004111 <j3004111@hotmail.com>
2020-09-13 01:58:42 +0800

    Create work01.py

---
+++ work01.py
@@ -0,0 +1,6 @@
+# import 是 Python 載入套件的基本語法 (類似 C 語言的 include), 後面接要載入的套件
+# import AAAAA as BB, 其中 BB 是代稱, 表示除了載入 AAAAA 之外, 之後都可以用 BB 代替 AAAAA 這個名稱
+# 常用套件往往有其對應代稱, numpy的代稱是np, pandas的代稱是pd, matplotlib.pyplot的代稱是plt
+# numpy 常用於數值/陣列運算, pandas 擅長資料格式的調整, matplotlib 擅長繪圖
+import numpy as np
+import matplotlib.pyplot as plt
